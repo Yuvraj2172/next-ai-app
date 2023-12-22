a41c7f6f10e655ecfa6a0c606ec9694ae3902670
Yuvraj Soni <demonsroar2172@gmail.com>
2023-12-22 19:03:27 +0530

    creating nav

---
+++ components/Nav.jsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+const Nav = () => {
+  return <nav className=""></nav>;
+};
+
+export default Nav;
